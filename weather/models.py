90381818bea3e46de986de2e46885e222511c293
Coconut <hamadehmy@gmail.com>
2026-01-19 21:11:58 +0400

    Add database indexes to Weather model

--- weather/models.py
+++ weather/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
-# Create your models here.
 class Weather(models.Model):
-    city_name = models.CharField(max_length = 100, unique = True)
+    city_name = models.CharField(max_length=100, unique=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    temperature = models.FloatField(null = True, blank = True)
-    windspeed = models.FloatField(null = True, blank = True)
-    winddirection = models.FloatField(null = True, blank = True)
-    weathercode = models.FloatField(null = True, blank = True)
-    time = models.CharField(max_length=50, null = True, blank = True)
-    
-    raw_payload = models.JSONField(null = True, blank = True)
-    synced_at = models.DateTimeField(null = True, blank = True)
-    
+    temperature = models.FloatField(null=True, blank=True)
+    windspeed = models.FloatField(null=True, blank=True)
+    winddirection = models.FloatField(null=True, blank=True)
+    weathercode = models.FloatField(null=True, blank=True)
+    time = models.CharField(max_length=50, null=True, blank=True)
+
+    raw_payload = models.JSONField(null=True, blank=True)
+    synced_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["synced_at"]),
+            models.Index(fields=["city_name", "synced_at"]),
+        ]
+
+
     def __str__(self):
         return self.city_name
